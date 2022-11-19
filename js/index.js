aa2d4c69089899b827fe72c9d21d894168100139
ttttttt <1299438995@qq.com>
2022-11-19 16:38:21 +0800

    第六周项目文件（首页交互效果和返回顶部）

--- js/index.js
+++ js/index.js
@@ -36,15 +36,15 @@
     });
 
     /** 电子书table切换 */ 
-    var $lis = $('.books .books-nav > li');
-    // 给自己添加激活类active 把兄弟都干掉
-    $(this).addClass('active').siblings().removeClass('active');
-    // 获取index
-    var index = $(this).index();
-    // 选中内容
-    var $ebooksList = $('.ebooks-list');
-    // 显示索引等于当前index
-    $ebooksList.eq(index).show().siblings('.ebooks-list').hide();
+    $('.books-nav > li').mouseenter(function(){
+        //导航高亮颜色切换
+        $(this).addClass('active').siblings('li').removeClass('active');
+        //获取索引
+        var index = $(this).index();
+        //内容切换
+        $('.ebooksList').eq(index).show().siblings('.ebooks-list').hide();
+    })
+    
 
     /** 新书列表手风琴效果 */
     $('.books .right-box ul>li').mouseenter(function(){
@@ -57,4 +57,104 @@
         $(this).find('.desc').show();
     })
 
- })
+    /** 服装轮播 */
+    $('#clothes-banner').tyslide({
+        boxh:336,//盒子的高度
+        w:472,//盒子的宽度
+        h:336,//图片的高度
+        isShow:true,//是否显示控制器
+        isShowBtn:true,//是否显示左右按钮
+        controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW:20,//控制按钮宽度
+        controlsH:4,//控制按钮高度
+        controlsColor:"#d7d7d7",//普通控制按钮的颜色
+        controlsCurrentColor:"#ff6600",//当前控制按钮的颜色
+    })
+
+    /* 童装小轮播 */
+    $("#children-banner").tyslide({
+        boxh:352,//盒子的高度
+        w:472,//盒子的宽度
+        h:352,//图片的高度
+        isShow:true,//是否显示控制器
+        isShowBtn:true,//是否显示左右按钮
+        controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW:20,//控制按钮宽度
+        controlsH:4,//控制按钮高度
+        controlsColor:"#d7d7d7",//普通控制按钮的颜色
+        controlsCurrentColor:"#f60000",//当前控制按钮的颜色
+    });
+
+    /* 户外运动小轮播 */ 
+    $("#sport-banner").tyslide({
+        boxh:352,//盒子的高度
+        w:472,//盒子的宽度
+        h:352,//图片的高度
+        isShow:true,//是否显示控制器
+        isShowBtn:true,//是否显示左右按钮
+        controltop:10,//控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW:20,//控制按钮宽度
+        controlsH:4,//控制按钮高度
+        controlsColor:"#d7d7d7",//普通控制按钮的颜色
+        controlsCurrentColor:"#f60000",//当前控制按钮的颜色
+    });
+
+    /** 服装tab切换 */
+    $('.clothes .clothes-nav li').mouseenter(function(){
+        //导航切换
+        $(this).addClass('active').siblings('li').removeClass('active');
+        //获取索引
+        var index = $(this).index();
+        //内容切换
+        $('.cloth .clothes-content > .clothes-list').eq(index).show().siblings('.clothes-list').hide();
+    })
+    
+    /** 户外运动tab切换 */
+    $('.sport .sport-nav li').mouseenter(function(){
+        //导航切换
+        $(this).addClass('active').siblings('li').removeClass('active');
+        //获取索引
+        var index = $(this).index();
+        //内容切换
+        $('.sports .clothes-content > .clothes-list').eq(index).show().siblings('.clothes-list').hide();
+    })
+
+    /** 童装tab切换 */
+    $('.children-clothes .clothes-nav li').mouseenter(function(){
+        //导航切换
+        $(this).addClass('active').siblings('li').removeClass('active');
+        //获取索引
+        var index = $(this).index();
+        //内容切换
+        $('.children-clothes .clothes-content > .clothes-list').eq(index).show().siblings('.clothes-list').hide();
+    })
+
+    /** 家居日用tab切换 */
+    $('.daily .clothes-nav li').mouseenter(function(){
+        //导航切换
+        $(this).addClass('active').siblings('li').removeClass('active');
+        //获取索引
+        var index = $(this).index();
+        //内容切换
+        $('.daily .clothes-content > .clothes-list').eq(index).show().siblings('.clothes-list').hide();
+    })
+
+
+    
+    /** 推广商品交互切换 */
+    $('.promotion .promotion-title ul li').mouseenter(function(){
+        // 导航激活类的切换
+        $(this).addClass('active').siblings().removeClass('active')
+        // 内容切换
+        // 获取对应的索引
+        console.log(index)
+        console.log($('.promotion .promotion-content .inner-box'))
+        var index = $(this).index(); // 0 => left:0*1170, 1 => left:-1*1170, 3 =>left:-2*1170
+        //左右移动
+        $('.promotion .promotion-content .inner-box').animate({
+            'left':-index*1170
+        },300)
+    })
+    
+})
+
